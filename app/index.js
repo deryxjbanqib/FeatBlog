8eff721e3cb43615f7a4ebad93df682952e83678
FeatBlog <deryxjbanqib@outlook.com>
2022-06-12 01:56:48 +0800

    update app/index.js.

--- app/index.js
+++ app/index.js
@@ -8,21 +8,23 @@ router.get('/:page?/:id?', async (ctx, next) => {
   const number = /^\+?[1-9][0-9]*$/
   const db = app.database()
   const $ = db.command.aggregate
-  const nav = await db.collection("feat-blog-forum").orderBy("c_order", "asc").get()
-  const config = await db.collection("feat-blog-config").get()
+  const config = await db.collection('feat-blog-config').get()
   const t = config.data[0]
   typeof ctx.params.page === 'undefined' ? t['page'] = 1 : t['page'] = ctx.params.page
-  const forum = await db.collection("feat-blog-forum").where({ route: t['page'] }).get()
-  const thread = await db.collection("feat-blog-thread").where({ _id: t['page'] }).get()
+  const nav = await db.collection('feat-blog-forum').orderBy('c_order', 'asc').get()
+  const top = await db.collection('feat-blog-thread').where({ top: true }).limit(t['top']).get()
+  const hot = await db.collection('feat-blog-thread').where({ hot: true }).limit(t['hot']).get()
+  const forum = await db.collection('feat-blog-forum').where({ route: t['page'] }).get()
+  const thread = await db.collection('feat-blog-thread').where({ _id: t['page'] }).get()
   t['match'] = {}
   t['view'] = 'index'
   t['skip'] = t['limit'] * (t['page'] - 1)
-  t['count'] = await db.collection("feat-blog-thread").count()
+  t['count'] = await db.collection('feat-blog-thread').count()
   if (forum.data.length == 0) {
     if (thread.data.length == 0) {
       if (!number.test(t['page'])) {
         t['match'] = { keywords: db.RegExp({ regexp: '.*' + ctx.params.page, options: 'i' }) }
-        t['count'] = await db.collection("feat-blog-thread").where(t['match']).count()
+        t['count'] = await db.collection('feat-blog-thread').where(t['match']).count()
         t['skip'] = t['limit'] * (common.getNan(ctx.params.id) - 1)
         t['href'] = ctx.params.page + '/'
         t['name'] = ctx.params.page
@@ -38,11 +40,11 @@ router.get('/:page?/:id?', async (ctx, next) => {
       t['description'] = thread.data[0].description
     }
   } else {
-    t['count'] = await db.collection("feat-blog-thread").where({ c_id: forum.data[0]._id }).count()
+    t['count'] = await db.collection('feat-blog-thread').where({ c_id: forum.data[0]._id }).count()
     t['skip'] = t['limit'] * (common.getNan(ctx.params.id) - 1)
     t['match'] = { route: ctx.params.page }
     t['href'] = ctx.params.page + '/'
-    t['pic'] = forum.data[0].c_pic
+    t['logo'] = forum.data[0].c_pic
     t['name'] = forum.data[0].name
     t['title'] = forum.data[0].name
     t['msg'] = forum.data[0].c_description
@@ -76,6 +78,8 @@ router.get('/:page?/:id?', async (ctx, next) => {
     await ctx.render(t['view'], {
       t,
       nav,
+      top,
+      hot,
       data,
       common
     })
